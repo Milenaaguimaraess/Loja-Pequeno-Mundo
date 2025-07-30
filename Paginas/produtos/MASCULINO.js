9189523c0e65e01eb81b1988a3b44c6663b7018e
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-07-30 17:19:54 -0300

    Upload de Projeto

---
+++ Paginas/produtos/MASCULINO.js
@@ -0,0 +1,172 @@
+const produtos = [
+  {
+    id: 1,
+    nome: "Camisa Estampada",
+    preco: "R$ 49,90",
+    descricao: "Camisa de algodão com estampa divertida para meninos.",
+    imagem: "img/masculino/1.png",
+    categoria: "menino",
+    tamanhos: ["P", "M", "G"],
+    cores: ["#f072a7", "#65be11", "#333"]
+  },
+  {
+    id: 2,
+    nome: "Bermuda Jeans",
+    preco: "R$ 59,90",
+    descricao: "Bermuda jeans confortável ideal para dias de verão.",
+    imagem: "img/masculino/2.png",
+    categoria: "menino",
+    tamanhos: ["4", "6", "8", "10"],
+    cores: ["#004080", "#555"]
+  },
+  {
+    id: 3,
+    nome: "Conjunto Esportivo",
+    preco: "R$ 79,90",
+    descricao: "Conjunto leve com camiseta e short para brincadeiras.",
+    imagem: "img/masculino/3.png",
+    categoria: "menino",
+    tamanhos: ["6", "8", "10", "12"],
+    cores: ["#000", "#f00"]
+  },
+  {
+    id: 4,
+    nome: "Jaqueta Jeans",
+    preco: "R$ 89,90",
+    descricao: "Jaqueta estilosa ideal para os dias frios.",
+    imagem: "img/masculino/4.png",
+    categoria: "menino",
+    tamanhos: ["8", "10", "12", "14"],
+    cores: ["#3a3a3a", "#1e90ff"]
+  },
+  {
+    id: 5,
+    nome: "Camiseta Básica",
+    preco: "R$ 29,90",
+    descricao: "Camiseta básica de algodão para o dia a dia.",
+    imagem: "img/masculino/5.png",
+    categoria: "menino",
+    tamanhos: ["P", "M", "G"],
+    cores: ["#fff", "#000"]
+  },
+  {
+    id: 6,
+    nome: "Shorts Colorido",
+    preco: "R$ 39,90",
+    descricao: "Shorts leve e colorido para os dias quentes.",
+    imagem: "img/masculino/6.png",
+    categoria: "menino",
+    tamanhos: ["4", "6", "8"],
+    cores: ["#ff6347", "#00ced1"]
+  },
+  {
+    id: 7,
+    nome: "Calça Moletom",
+    preco: "R$ 69,90",
+    descricao: "Calça de moletom confortável para o inverno.",
+    imagem: "img/masculino/7.png",
+    categoria: "menino",
+    tamanhos: ["6", "8", "10"],
+    cores: ["#a9a9a9", "#2f4f4f"]
+  },
+  {
+    id: 8,
+    nome: "Camisa Polo",
+    preco: "R$ 54,90",
+    descricao: "Camisa polo clássica para ocasiões especiais.",
+    imagem: "img/masculino/8.png",
+    categoria: "menino",
+    tamanhos: ["P", "M", "G"],
+    cores: ["#4682b4", "#b22222"]
+  },
+  {
+    id: 9,
+    nome: "Conjunto Pijama",
+    preco: "R$ 44,90",
+    descricao: "Pijama confortável com estampa divertida.",
+    imagem: "img/masculino/9.png",
+    categoria: "menino",
+    tamanhos: ["4", "6", "8", "10"],
+    cores: ["#f5f5f5", "#00008b"]
+  },
+  {
+    id: 10,
+    nome: "Camiseta Listrada",
+    preco: "R$ 34,90",
+    descricao: "Camiseta com listras estilosas.",
+    imagem: "img/masculino/10.png",
+    categoria: "menino",
+    tamanhos: ["6", "8", "10", "12"],
+    cores: ["#ffeb3b", "#2196f3"]
+  },
+  {
+    id: 11,
+    nome: "Bermuda Estampada",
+    preco: "R$ 49,90",
+    descricao: "Bermuda com estampa tropical.",
+    imagem: "img/masculino/11.png",
+    categoria: "menino",
+    tamanhos: ["6", "8", "10", "12"],
+    cores: ["#32cd32", "#ff4500"]
+  },
+  {
+    id: 12,
+    nome: "Regata Esportiva",
+    preco: "R$ 24,90",
+    descricao: "Regata ideal para praticar esportes.",
+    imagem: "img/masculino/12.png",
+    categoria: "menino",
+    tamanhos: ["P", "M", "G"],
+    cores: ["#1e90ff", "#ffffff"]
+  },
+  {
+    id: 13,
+    nome: "Calça Jeans",
+    preco: "R$ 74,90",
+    descricao: "Calça jeans com ajuste confortável.",
+    imagem: "img/masculino/13.png",
+    categoria: "menino",
+    tamanhos: ["8", "10", "12", "14"],
+    cores: ["#00008b", "#2f4f4f"]
+  },
+  {
+    id: 14,
+    nome: "Camisa Xadrez",
+    preco: "R$ 64,90",
+    descricao: "Camisa xadrez para um visual descolado.",
+    imagem: "img/masculino/14.png",
+    categoria: "menino",
+    tamanhos: ["6", "8", "10"],
+    cores: ["#8b0000", "#ffa07a"]
+  },
+  {
+    id: 15,
+    nome: "Conjunto de Inverno",
+    preco: "R$ 94,90",
+    descricao: "Casaco e calça para os dias frios.",
+    imagem: "img/masculino/15.png",
+    categoria: "menino",
+    tamanhos: ["8", "10", "12"],
+    cores: ["#708090", "#b0c4de"]
+  },
+  {
+    id: 16,
+    nome: "Camiseta com Personagem",
+    preco: "R$ 39,90",
+    descricao: "Camiseta com personagem favorito da criançada.",
+    imagem: "img/masculino/16.png",
+    categoria: "menino",
+    tamanhos: ["4", "6", "8"],
+    cores: ["#ffa500", "#ff69b4"]
+  },
+  {
+    id: 17,
+    nome: "Conjunto Básico",
+    preco: "R$ 49,90",
+    descricao: "Camiseta e bermuda para o dia a dia.",
+    imagem: "img/masculino/17.png",
+    categoria: "menino",
+    tamanhos: ["P", "M", "G"],
+    cores: ["#87cefa", "#000"]
+  }
+];
